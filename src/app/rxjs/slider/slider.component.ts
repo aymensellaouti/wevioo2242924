9ed086c0f4c1bdef13ae2134c8c730ddaf20b669
aymensellaouti <aymen.sellaouti@gmail.com>
2024-04-23 12:09:24 +0100

    exercice slider

--- src/app/rxjs/slider/slider.component.ts
+++ src/app/rxjs/slider/slider.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { map, startWith, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-slider',
@@ -6,8 +7,8 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./slider.component.css'],
 })
 export class SliderComponent {
-
   @Input() timer = 1000;
+  @Input() size = 150;
   @Input()
   paths = [
     'as.jpg',
@@ -16,4 +17,13 @@ export class SliderComponent {
     'rotating_card_profile2.png',
     'rotating_card_profile3.png',
   ];
+  paths$ = timer(0, 1000).pipe(
+    // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ...
+    tap((data) => console.log(data)),
+    map((index) => this.paths[index % this.paths.length]),
+    tap((data) => console.log(data)),
+    startWith(this.paths[this.paths.length - 1])
+
+    //'as.jpg',  'cv.png','rotating_card_profile.png',
+  );
 }
