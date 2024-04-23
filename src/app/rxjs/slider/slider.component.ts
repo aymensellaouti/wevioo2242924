f4b38aad8374d3aa3bc82f2e86c59aef2949da25
aymensellaouti <aymen.sellaouti@gmail.com>
2024-04-23 15:40:56 +0100

    slider with combineLatest

--- src/app/rxjs/slider/slider.component.ts
+++ src/app/rxjs/slider/slider.component.ts
@@ -1,5 +1,16 @@
-import { Component, Input } from '@angular/core';
-import { map, startWith, tap, timer } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, inject } from '@angular/core';
+import { Observable, combineLatest, map, startWith, tap, timer } from 'rxjs';
+import { API } from 'src/config/api.config';
+
+export interface Image {
+  albumId: number
+  id: number
+  title: string
+  url: string
+  thumbnailUrl: string
+}
+
 
 @Component({
   selector: 'app-slider',
@@ -17,13 +28,18 @@ export class SliderComponent {
     'rotating_card_profile2.png',
     'rotating_card_profile3.png',
   ];
+  http = inject(HttpClient);
+  images$ = this.http.get<Image[]>(API.photos);
   paths$ = timer(0, 1000).pipe(
     // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ...
-    tap((data) => console.log('after timer : '+data)),
+    tap((data) => console.log('after timer : ' + data)),
     map((index) => this.paths[index % this.paths.length]),
-    tap((data) => console.log('after map : '+data)),
+    tap((data) => console.log('after map : ' + data)),
     startWith(this.paths[this.paths.length - 1])
-
     //'as.jpg',  'cv.png','rotating_card_profile.png',
   );
+  imagesPaths$: Observable<string> = combineLatest([
+    timer(0, 1000),
+    this.images$,
+  ]).pipe(map(([index, images]) => images[index % images.length].url));
 }
